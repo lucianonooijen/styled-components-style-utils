3c9b18fd0ff3eb4a98a2708fe1b8dea71d6c8dca
Luciano Nooijen <lucianonooijen@gmail.com>
2018-08-13 22:07:48 +0200

    Added index file

---
+++ index.js
@@ -0,0 +1,11 @@
+const reset = require('./src/reset');
+const debug = require('./src/debug');
+const center = require('./src/center');
+const truncate = require('./src/truncate');
+
+module.exports = {
+    reset,
+    debug,
+    center,
+    truncate,
+};
